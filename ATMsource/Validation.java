58c82905b0b9c59c1efbf526844219e1951a39d8
0147123 <9536442@gmail.com>
2021-10-31 22:50:57 +0800

    Update - check int

---
+++ ATMsource/Validation.java
@@ -0,0 +1,18 @@
+public class Validation {
+    Screen screen;
+    public Validation(Screen thescreen){
+        screen = thescreen;
+    }
+
+    //boolean method - check data type is integer
+    public int checkInt (String check){
+        try {
+            int buffer = Integer.parseInt(check);
+            return buffer;
+        } catch (Exception e) {
+            screen.displayMessageLine("\tInvalid data type: input should be integer!");
+            return -1;
+        }
+    }
+
+}
