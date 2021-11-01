173ea418a43e9f28fcfd1a68a449f741010776c3
0147123 <9536442@gmail.com>
2021-11-01 21:32:07 +0800

    Update Validation.java

--- ATMsource/Validation.java
+++ ATMsource/Validation.java
@@ -11,12 +11,12 @@ public class Validation {
     //Return integer method - check data type is integer
     public int checkInt (String check){
         try {
-            //input is double, return false
+            //input is double, return -1
             if(check.contains(".")){
                 screen.displayMessageLine("\tInvalid data type: input should be integer!");
                 return -1;  //-1 mean invalid input
             }
-            //input digits that larger than 9, return false
+            //input digits that larger than 9, return -1
             if (check.length() > 9){
                 screen.displayMessageLine("\tInvalid data length: input should be within 9 digits!");
                 return -1;  //-1 mean invalid input
