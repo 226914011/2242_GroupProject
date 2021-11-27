c09409fa468b71057ed2718621216d2e4222f48e
lshcyrus <lshcyrus1228@gmail.com>
2021-11-28 06:52:31 +0800

    transferConfirm, transferUpdate

--- to egg_ATMsource in my vscode (sooooo many bugs)/transferComfirm.java
+++ to egg_ATMsource in my vscode (sooooo many bugs)/transferComfirm.java
@@ -1,11 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
@@ -13,10 +10,16 @@ import javax.swing.JButton;
 
 public class TransferComfirm extends Screen {
 
-	private JPanel contentPane;
+	private DisplayMessageJPanel transferConfirmPanel;
 	private JTextField accountNumTextField;
 	private JTextField transferAmountTtextField;
+	private JLabel accountNumLabel;
+	private JLabel transferAmountLabel;
+	private JButton transferConfirmButton;
+	private JButton transferReEnterButton;
+	private JButton btnCancel;
 
+<<<<<<< Updated upstream:to egg_ATMsource in my vscode (sooooo many bugs)/transferComfirm.java
 
 	/**
 	 * Create the frame.
@@ -26,71 +29,68 @@ public class TransferComfirm extends Screen {
 		contentPane = new JPanel();
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+=======
+	public void buildGUI(){
+	transferConfirmPanel = new DisplayMessageJPanel();
+>>>>>>> Stashed changes:to egg_ATMsource in my vscode (sooooo many bugs)/TransferComfirm.java
 		
-		//need to change when copy to vscode
-		JPanel panel = new JPanel();
-        getContentPane().add(panel);
-        //setPreferredSize(new Dimension(1440, 1024));
-        
-
-		contentPane.add(panel, BorderLayout.CENTER);
-		panel.setPreferredSize(new Dimension(1440, 1024));
-		panel.setLayout(null);
+	super.getMainframe().getContentPane().add(transferConfirmPanel);
+	super.getScreenContentPane().add(transferConfirmPanel, BorderLayout.CENTER);
 		
-		JLabel accountNumLabel = new JLabel("The account number for Transfer:");
+		accountNumLabel = new JLabel("The account number for Transfer:");
 		accountNumLabel.setFont(new Font("Arial", Font.PLAIN, 40));
 		accountNumLabel.setBounds(50, 313, 605, 82);
-		panel.add(accountNumLabel);
+		transferConfirmPanel.add(accountNumLabel);
 		
-		JLabel transferAmountLabel = new JLabel("Your transfer amount:");
+		transferAmountLabel = new JLabel("Your transfer amount:");
 		transferAmountLabel.setFont(new Font("Arial", Font.PLAIN, 40));
 		transferAmountLabel.setBounds(50, 531, 925, 85);
-		panel.add(transferAmountLabel);
+		transferConfirmPanel.add(transferAmountLabel);
 		
 		accountNumTextField = new JTextField();
 		accountNumTextField.setEditable(false);
 		accountNumTextField.setFont(new Font("Arial", Font.PLAIN, 40));
 		accountNumTextField.setBounds(730, 313, 524, 85);
-		panel.add(accountNumTextField);
 		accountNumTextField.setColumns(10);
+		transferConfirmPanel.add(accountNumTextField);
+		
 		
 		transferAmountTtextField = new JTextField();
 		transferAmountTtextField.setFont(new Font("Arial", Font.PLAIN, 40));
 		transferAmountTtextField.setEditable(false);
 		transferAmountTtextField.setColumns(10);
 		transferAmountTtextField.setBounds(730, 531, 524, 85);
-		panel.add(transferAmountTtextField);
+		transferConfirmPanel.add(transferAmountTtextField);
 		
-		//y is 836
-		JButton transferConfirmButton = new JButton("Confirm");
+		transferConfirmButton = new JButton("Confirm");
 		transferConfirmButton.setFont(new Font("Arial Black", Font.PLAIN, 40));
 		transferConfirmButton.setBounds(166, 836, 292, 84);
-		panel.add(transferConfirmButton);
+		transferConfirmPanel.add(transferConfirmButton);
 		
-		JButton transferReEnterButton = new JButton("Re-enter");
+		transferReEnterButton = new JButton("Re-enter");
 		transferReEnterButton.setFont(new Font("Arial Black", Font.PLAIN, 40));
 		transferReEnterButton.setBounds(574, 836, 292, 84);
-		panel.add(transferReEnterButton);
+		transferConfirmPanel.add(transferReEnterButton);
 		
-		JButton btnCancel = new JButton("Cancel");
+		btnCancel = new JButton("Cancel");
 		btnCancel.setFont(new Font("Arial Black", Font.PLAIN, 40));
 		btnCancel.setBounds(982, 836, 292, 84);
-		panel.add(btnCancel);
+		transferConfirmPanel.add(btnCancel);
 		
-		
-		pack();
+		super.getMainframe().pack();
+		super.getMainframe().repaint();
+	}
+
+	public JButton getTransferConfirmButton(){
+		return transferConfirmButton;
+	}
+
+	public JButton getTransferReEnterButton(){
+		return transferReEnterButton;
+	}
+
+	public JButton getBtnCancelButton(){
+		return btnCancel;
 	}
 	
-	
-	public class DisplayMessageJPanel extends JPanel{
-	    public DisplayMessageJPanel(){
-	        setSize(new Dimension(1440, 1024));
-	        //Absolute layout
-	        setLayout(null);
-
-
-	        //background with color
-	        setBackground(new Color(56, 60, 102));
-	    }
-	}
 }
