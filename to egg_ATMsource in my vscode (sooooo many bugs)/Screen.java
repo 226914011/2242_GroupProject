6ab5306463b5699627e7e5a7ff9b5a2a098d053f
0147123 <9536442@gmail.com>
2021-11-28 01:16:59 +0800

    Update - screen JFrame use static

--- to egg_ATMsource in my vscode (sooooo many bugs)/Screen.java
+++ to egg_ATMsource in my vscode (sooooo many bugs)/Screen.java
@@ -14,6 +14,11 @@ public class Screen extends JFrame
    //GUI componnent
 	private static JFrame Mainframe;
 	private static JPanel contentPane;
+	public JPanel mainmenupanel;
+	public JButton balanceButton;
+	public JButton withdrawButton;
+	public JButton transferButton;
+	public JButton exitButton;
 
 
 	public Screen(){
@@ -26,6 +31,37 @@ public class Screen extends JFrame
 		//set panel size
         contentPane.setPreferredSize(new Dimension(1440, 1024));
 	}
+
+	public void menuGUI(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 1000);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		mainmenupanel = new JPanel();
+		mainmenupanel.setSize(1000, 1000);
+        getContentPane().add(mainmenupanel);
+		contentPane.add(mainmenupanel, BorderLayout.CENTER);
+		mainmenupanel.setLayout(null);
+		
+		balanceButton = new JButton("1. View my balance");
+		balanceButton.setBounds(234, 300, 532, 100);
+		balanceButton.setFont(new Font("Arial Black", Font.PLAIN, 40));
+		mainmenupanel.add(balanceButton);
+		
+		withdrawButton = new JButton("2. Withdraw");
+		withdrawButton.setBounds(234, 450, 532, 100);
+		withdrawButton.setFont(new Font("Arial Black", Font.PLAIN, 40));
+		mainmenupanel.add(withdrawButton);
+		
+		transferButton = new JButton("3. Transfer");
+		transferButton.setBounds(234, 600, 532, 100);
+		transferButton.setFont(new Font("Arial Black", Font.PLAIN, 40));
+		mainmenupanel.add(transferButton);
+		
+		exitButton = new JButton("4. Exit");
+		exitButton.setFont(new Font("Arial Black", Font.PLAIN, 40));
 
 
    // displays a message without a carriage return
