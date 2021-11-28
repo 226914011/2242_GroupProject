79fbbf0f2b78eab1a4619b1a9b4938d085aa9559
0147123 <9536442@gmail.com>
2021-11-28 21:53:15 +0800

    GUI - Dispylay message

---
+++ to egg_ATMsource in my vscode (sooooo many bugs)/InsertPagePanel.java
@@ -0,0 +1,21 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class InsertPagePanel extends JPanel{
+    public InsertPagePanel(String displayText){
+        setPreferredSize(new Dimension(890, 1024));
+        setLayout(null);
+        
+        JLabel messageLabel = new JLabel(displayText);
+        messageLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+        messageLabel.setBounds(50, 339, 840, 92);
+        add(messageLabel);
+
+        //background with color
+        setBackground(new Color(56, 60, 102));
+    }
+}
