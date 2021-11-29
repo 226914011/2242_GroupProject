79b17cb380698851b0315ac4654848268f85e73c
226914011 <wongtianyouemail@gmail.com>
2021-11-29 12:23:28 +0800

    GUI - Color Corr

--- to egg_ATMsource in my vscode (sooooo many bugs)/TransferUpdate.java
+++ to egg_ATMsource in my vscode (sooooo many bugs)/TransferUpdate.java
@@ -3,6 +3,7 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.Color;
 
 public class TransferUpdate extends Screen {
 
@@ -22,26 +23,31 @@ public class TransferUpdate extends Screen {
 
 		transferSuccessLabel = new JLabel("The transfer is successful. Updated balance:");
 		transferSuccessLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+		transferSuccessLabel.setForeground(Color.LIGHT_GRAY);
 		transferSuccessLabel.setBounds(50, 104, 1204, 85);
 		transferUpdatePanel.add(transferSuccessLabel);
 		
 		availableBalanceLabel = new JLabel("available balance:");
 		availableBalanceLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+		availableBalanceLabel.setForeground(Color.LIGHT_GRAY);
 		availableBalanceLabel.setBounds(50, 308, 532, 85);
 		transferUpdatePanel.add(availableBalanceLabel);
 		
 		totalBalanceLabel = new JLabel("total balance:");
 		totalBalanceLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+		totalBalanceLabel.setForeground(Color.LIGHT_GRAY);
 		totalBalanceLabel.setBounds(50, 517, 532, 85);
 		transferUpdatePanel.add(totalBalanceLabel);
 		
 		availableBtxtfield = new JTextField();
+		totalBalanceLabel.setFont(new Font("Arial", Font.PLAIN, 40));
 		availableBtxtfield.setBounds(730, 313, 524, 85);
 		availableBtxtfield.setColumns(10);
 		availableBtxtfield.setEditable(false);
 		transferUpdatePanel.add(availableBtxtfield);
 		
 		totalBtxtfield = new JTextField();
+		totalBalanceLabel.setFont(new Font("Arial", Font.PLAIN, 40));
 		totalBtxtfield.setColumns(10);
 		totalBtxtfield.setBounds(730, 523, 524, 85);
 		totalBtxtfield.setEditable(false);
